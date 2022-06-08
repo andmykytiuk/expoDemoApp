b6d3920cc494fd55debcd921a916ece16cf26b7d
Andrew Mykytiuk <andrew.mykytiuk@mati.io>
2022-06-08 11:45:49 +0200

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Button,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {
+  MetaMapRNSdk,
+} from 'react-native-expo-metamap-sdk';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    console.log('Constructor Called.');
+  }
+
+  componentDidMount() {
+	 //set listening callbacks
+  	const MetaMapVerifyResult = new NativeEventEmitter(NativeModules.MetaMapRNSdk)
+ 	 MetaMapVerifyResult.addListener('verificationSuccess', (data) => console.log(data))
+ 	 MetaMapVerifyResult.addListener('verificationCanceled', (data) => console.log(data))
+  }
+
+  //call showFlow when button is clicked
+  handleMetaMapClickButton = () => {
+
+	 //set 3 params clientId (cant be null), flowId, metadata
+  	  var yourMetadata = { param1: "value1", param2: "value2" }
+
+   	 MetaMapRNSdk.showFlow("6109011657eea7001ca489f9", "61128ba28910d4001b944de4", yourMetadata);
+  }
+
+  //Add button to view graph
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'powderblue',
+        }}>
+        <Button onPress={this.handleMetaMapClickButton} title="Click here"/>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
